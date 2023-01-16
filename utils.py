2f43e1bf1ce831d523500a235d84bdccfb32fef6
stone4022 <49816448+stone4022@users.noreply.github.com>
2023-01-16 14:01:38 +0800

    updated log

--- utils.py
+++ utils.py
@@ -150,7 +150,7 @@ class SeqtoText:
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, save_path, patience=10, verbose=True, delta=0.1):
+    def __init__(self, save_path, patience=10, verbose=True, delta=0):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
